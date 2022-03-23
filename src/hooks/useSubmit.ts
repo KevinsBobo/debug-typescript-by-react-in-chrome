328c6f0ad96134194c5814abb77fb5384561ca2d
KevinsBobo <kevins.bobo@gmail.com>
2022-03-23 11:09:25 +0800

    update

--- src/hooks/useSubmit.ts
+++ src/hooks/useSubmit.ts
@@ -1,5 +1,5 @@
 import { clusterApiUrl, Connection, Keypair, LAMPORTS_PER_SOL, PublicKey } from '@solana/web3.js';
-import { AccountLayout, approveChecked, createMint, getAccount, getMint, getOrCreateAssociatedTokenAccount, mintTo, TOKEN_PROGRAM_ID, transfer } from '@solana/spl-token'; // @FIXME: replace with @solana/spl-token
+import { AccountLayout, approve, approveChecked, createMint, getAccount, getMint, getOrCreateAssociatedTokenAccount, mintTo, TOKEN_PROGRAM_ID, transfer } from '@solana/spl-token'; // @FIXME: replace with @solana/spl-token
 import bs58 from 'bs58'
 
 
@@ -12,22 +12,108 @@ export const useSubmit = () => {
 function submit() {
     // createMintAndTransferTokens()
     // approveAndTransferTokens()
+    // checkApproved(new PublicKey('9FDCTmKNstoKwMSVAUWwnUv3mbqRFyQGDDRdqB5eLPsN'))
+    // checkMint(new PublicKey('8zRnXoSK6To5PxmrkneuGPBwhfAxrnAjbYKguQsKSpeq'))
+    // testGetAssociatedTokenAccount()
+    // approveTokens()
     checkApproved(new PublicKey('9FDCTmKNstoKwMSVAUWwnUv3mbqRFyQGDDRdqB5eLPsN'))
+    checkAccount()
 }
 
 function getAccountFromSecretKey(sercetKey: string): Keypair {
     return Keypair.fromSecretKey(bs58.decode(sercetKey))
 }
 
-
 async function checkApproved(account: PublicKey) {
     // Connect to cluster
     const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
     const approvedAccountInfo = await getAccount(connection, account, undefined, TOKEN_PROGRAM_ID)
-    console.log(approvedAccountInfo.delegate)
+    console.log(approvedAccountInfo.delegate?.toString())
     console.log(approvedAccountInfo.delegatedAmount)
 }
 
+async function checkMint(mint: PublicKey) {
+    // Connect to cluster
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+    const mintInfo = await getMint(connection, mint)
+    console.log(mintInfo)
+}
+
+async function checkAccount() {
+    const fromWallet = getAccountFromSecretKey('5CJn8hnPKQ7KRo8D9YF4J98tt4C4bn7kgMXL6Bpc4H1srNqwmCTBNUcSfqWkBZReincY252rVDdfQReg7Jx7cztC')
+    const mintAuthority = getAccountFromSecretKey('JpAs3PCAC98Bx4ChfAbPzzd5deV96ozLu44wkCuAQhJwBdFtJcT3QA7ABoHKrJksdQgnBULpCb6QkVkhEDErCiQ')
+    const freezeAuthority = getAccountFromSecretKey('4f3kdJ6Ga5QXQSTrZxJ84T1YTafwh8caD8RweDb7KutKWc1kWsRGtphbaaqYWMFwgt2dFbUcdr959hSMKh34LFpN')
+    const toWallet = getAccountFromSecretKey('16sBQLdzGCXsv6uNXrH2tt7NbXnWuYAWr4ZUuWuvGrn7vRRFy39jdXgJCZrnL2L9gFv2wpS4L5WEeXNr4W7bpdm')
+    const testWallet = getAccountFromSecretKey('4wz9UVEFV8HFtXGHp8fPEMhPwzYVh384Jm1qpmim2r9pZfF7qSwhGRr8ShZK7UMNNX8eSPdn4sRbzSH88n5hi7XN')
+
+    const account = toWallet.publicKey
+    // Connect to cluster
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+    const testkenAccounts = await connection.getTokenAccountsByOwner(
+        account,
+        {
+            programId: TOKEN_PROGRAM_ID,
+        }
+    )
+
+    console.log("Token                                         Balance");
+    console.log("------------------------------------------------------------");
+    testkenAccounts.value.forEach((e) => {
+        const accountInfo = AccountLayout.decode(e.account.data);
+        console.log(`${new PublicKey(accountInfo.mint)}   ${accountInfo.amount}`);
+    })
+}
+
+async function testGetAssociatedTokenAccount() {
+    // Connect to cluster
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+    const fromWallet = getAccountFromSecretKey('5CJn8hnPKQ7KRo8D9YF4J98tt4C4bn7kgMXL6Bpc4H1srNqwmCTBNUcSfqWkBZReincY252rVDdfQReg7Jx7cztC')
+    const toWallet = getAccountFromSecretKey('16sBQLdzGCXsv6uNXrH2tt7NbXnWuYAWr4ZUuWuvGrn7vRRFy39jdXgJCZrnL2L9gFv2wpS4L5WEeXNr4W7bpdm')
+    const mint = new PublicKey('8zRnXoSK6To5PxmrkneuGPBwhfAxrnAjbYKguQsKSpeq')
+    let toTokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        fromWallet,
+        mint,
+        toWallet.publicKey
+    )
+    console.log(toTokenAccount) // .address.toString())
+
+    // toTokenAccount = await getOrCreateAssociatedTokenAccount(
+    //     connection,
+    //     fromWallet,
+    //     mint,
+    //     toTokenAccount.address
+    // )
+    // console.log(toTokenAccount.address.toString())
+}
+
+async function approveTokens() {
+    // Connect to cluster
+    const connection = new Connection(clusterApiUrl('devnet'), 'confirmed')
+
+    const fromWallet = getAccountFromSecretKey('5CJn8hnPKQ7KRo8D9YF4J98tt4C4bn7kgMXL6Bpc4H1srNqwmCTBNUcSfqWkBZReincY252rVDdfQReg7Jx7cztC')
+    const toWallet = getAccountFromSecretKey('16sBQLdzGCXsv6uNXrH2tt7NbXnWuYAWr4ZUuWuvGrn7vRRFy39jdXgJCZrnL2L9gFv2wpS4L5WEeXNr4W7bpdm')
+    const testWalletPublicKey = new PublicKey('77SsSbSVGvMx18tnHgDq9cRPxUuJr9gHFoWqceB8TiRn')
+    const mint = new PublicKey('8zRnXoSK6To5PxmrkneuGPBwhfAxrnAjbYKguQsKSpeq')
+
+    let toTokenAccount = await getOrCreateAssociatedTokenAccount(
+        connection,
+        fromWallet,
+        mint,
+        toWallet.publicKey
+    )
+    console.log(toTokenAccount.address.toString())
+
+    let signature = await approve(
+        connection,
+        fromWallet,
+        toTokenAccount.address,
+        testWalletPublicKey,
+        toWallet,
+        10 ** 18
+    )
+    console.log(`approveChecked tx ${signature}`)
+}
 
 async function approveAndTransferTokens() {
     // Connect to cluster
@@ -131,7 +217,13 @@ async function createMintAndTransferTokens() {
     console.log(walletBalance)
 
     // Create new token mint
-    const mint = await createMint(connection, fromWallet, mintAuthority.publicKey, freezeAuthority.publicKey, 9);
+    const mint = await createMint(
+        connection,
+        fromWallet,
+        mintAuthority.publicKey,
+        freezeAuthority.publicKey,
+        9
+    );
     console.log(mint.toBase58());
 
     // Get the token account of the fromWallet address, and if it does not exist, create it
